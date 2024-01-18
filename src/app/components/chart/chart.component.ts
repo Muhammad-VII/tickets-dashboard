99d6c14818567471c3d430e9ba4c3d2164930b2d
Mohammed Akmal <meshoronaldo62@gmail.com>
2024-01-19 00:45:58 +0200

    1.1.0

--- src/app/components/chart/chart.component.ts
+++ src/app/components/chart/chart.component.ts
@@ -6,7 +6,7 @@ import {
   afterNextRender,
   inject,
 } from '@angular/core';
-import type { EChartsOption } from 'echarts';
+import { graphic, type EChartsOption } from 'echarts';
 import { NgxEchartsDirective, provideEcharts } from 'ngx-echarts';
 
 @Component({
@@ -20,17 +20,62 @@ import { NgxEchartsDirective, provideEcharts } from 'ngx-echarts';
 export class ChartComponent {
   options: EChartsOption = {
     tooltip: {
-      trigger: 'item',
-      align: 'right',
+      trigger: 'axis',
+      axisPointer: {
+        type: 'line',
+        lineStyle: {
+          color: '#8A74F9',
+          type: 'solid',
+          width: 2,
+        },
+
+        label: {
+          backgroundColor: '#6a7985',
+        },
+      },
+      align: 'center',
+      triggerOn: 'mousemove',
+      position: function (pt: any) {
+        return [pt[0], '10%'];
+      },
+      backgroundColor: '#1E1B39',
+      formatter: (params: any) => {
+        console.log(params);
+        return `<div class="font-normal">
+        ${params[0].value} إستخدام <br> 
+        <div class="text-primary">
+        <i class="fa-solid fa-circle-arrow-up rotate-[45deg]"></i> 1.2%
+        </div>
+        </div> `;
+      },
+      textStyle: {
+        color: '#E5E5EF',
+        fontWeight: 500,
+      },
     },
     grid: {
       left: '3%',
       right: '3%',
       bottom: '5%',
+      top: '10%',
       containLabel: true,
     },
     xAxis: {
       boundaryGap: false,
+      axisLine: {
+        show: false,
+      },
+      axisTick: {
+        show: false,
+      },
+      axisLabel: {
+        show: true,
+        fontFamily: 'Neo Sans Arabic Regular',
+        fontSize: 16,
+        fontWeight: 600,
+        margin: 20,
+        align: 'left',
+      },
       data: [
         'ديسمبر',
         'نوفمبر',
@@ -39,17 +84,34 @@ export class ChartComponent {
         'أغسطس',
         'يوليو',
         'يونيه',
-        'ماير',
+        'مايو',
         'إبريل',
         'مارس',
         'فبراير',
         'يناير',
       ],
     },
-    yAxis: {},
+    yAxis: {
+      splitLine: {
+        show: true,
+        lineStyle: {
+          color: '#E5E5EF',
+          opacity: 1,
+        },
+      },
+      axisLine: {
+        show: false,
+      },
+      axisTick: {
+        show: false,
+      },
+      axisLabel: {
+        show: false,
+      },
+    },
     series: [
       {
-        data: [200, 300, 400, 300, 400, 300, 400, 500, 400, 300, 400, 300],
+        data: [50, 150, 110, 120, 200, 180, 220, 200, 170, 100, 50, 150],
         type: 'line',
         smooth: true,
         lineStyle: {
@@ -59,9 +121,20 @@ export class ChartComponent {
         itemStyle: {
           color: '#8A74F9',
         },
+        showSymbol: true,
+        symbol: 'circle',
+        symbolSize: 10,
+        
         areaStyle: {
-          color: '#8A74F9',
-          opacity: 0.6,
+          color: new graphic.LinearGradient(0, 0, 0, 1, [
+            {
+              offset: 0,
+              color: '#8a74f9bb'
+            },
+            {
+              offset: 1,
+              color: '#8a74f93a'
+            }])
         },
       },
     ],
--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { TicketComponent } from '../ticket/ticket.component';
 import { ChartComponent } from '../chart/chart.component';
+import { TableComponent } from '../table/table.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [TicketComponent, ChartComponent],
+  imports: [TicketComponent, ChartComponent, TableComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
